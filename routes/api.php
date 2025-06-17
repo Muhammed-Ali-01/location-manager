18fe38fad8368261c864bcb4c3540fdd35a80d31
ali <m.aligenel01@gmail.com>
2025-06-17 15:24:37 +0300

    add rate limit to api endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\LocationController;
+use App\Http\Controllers\Api\RoutingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\RoutingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('locations', LocationController::class);
+Route::middleware('throttle:10,1')->group(function () {
+    Route::apiResource('locations', LocationController::class);
+    Route::get('route/{location}', [RoutingController::class, 'index'])->name('route');
 
-Route::get('route/{location}', [RoutingController::class, 'index'])->name('route');
+});
