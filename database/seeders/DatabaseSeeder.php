9f0d0c1dc8d0066714c039f0903ec8ebd9cdc9e9
ali <m.aligenel01@gmail.com>
2025-06-19 08:57:29 +0300

    add location seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name'  => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(LocationSeeder::class);
     }
 }
